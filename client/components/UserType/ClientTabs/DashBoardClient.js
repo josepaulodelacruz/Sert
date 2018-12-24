358e6c7534c8241167874aced98402b97c8fe921
josepaulodelacruz <whoseepowlu@gmail.com>
2018-12-24 17:54:48 +0800

    working native-base integration

--- client/components/UserType/ClientTabs/DashBoardClient.js
+++ client/components/UserType/ClientTabs/DashBoardClient.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { Header, Left, Right } from 'native-base';
 
 class DashBoardClient extends Component {
 	render(){
 		return(
 			<View style={styles.container}>
-			  <Ionicons name="md-menu" size={32} color="green" />
+			<Header>
+				<Left>
+					<Ionicons name="md-menu" size={32} color="green" />
+				</Left>
+			</Header>
 				<Text>dashboard</Text>
 			</View>
 		)
