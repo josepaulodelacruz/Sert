f2d4292eb4139ed35f694f6a866c42fbd0bd8cc7
josepaulodelacruz <whoseepowlu@gmail.com>
2019-01-06 11:45:59 +0800

    updated Code

--- client/components/UserType/Locations/CurrentLocation.js
+++ client/components/UserType/Locations/CurrentLocation.js
@@ -8,7 +8,7 @@ class CurrentLocation extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			location: 'Location not Available',
+			location: '',
 			latitude: null,
 			longitude: null,
 		}
@@ -34,10 +34,11 @@ class CurrentLocation extends Component {
 		console.log(DataCoords.features[0].geometry.coordinates)
 		currentLocation = DataCoords.features[0].geometry.coordinates.map(coordinate => {
 			coordinate.forEach(place => {
-				if(this.state.longitude <= 121.14486694335936 && this.state.longitude <= 121.12718582153319 && this.state.latitude <= 14.285760476751552 && this.state.latitude <= 14.276777229686052) {
+				if(this.state.longitude === 0 && this.state.longitude === 0){
+					this.setState({ location: 'Location Not Available in your device' })
+					console.log('location not available at your device');
+				}else if(this.state.longitude <= 121.14486694335936 && this.state.longitude <= 121.12718582153319 && this.state.latitude <= 14.285760476751552 && this.state.latitude <= 14.276777229686052) {
 					this.setState({ location: 'Saint Joseph 6'})
-				}else{
-					this.setState({ location: `You're not in SJV`});
 				}
 			})
 		})
