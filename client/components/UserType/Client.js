efef6f32bf200e930f035c6a886052abf4db4a1e
josepaulodelacruz <whoseepowlu@gmail.com>
2019-01-21 17:30:58 +0800

    firebase database

--- client/components/UserType/Client.js
+++ client/components/UserType/Client.js
@@ -1,193 +1,172 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import { View, Text, StyleSheet, TextInput, Picker, ScrollView, Button} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from 'react-native';
+import { Container, Header, Content, Form, Item, Input, Label, Radio, ListItem, Left, Icon } from 'native-base';
+
+import Swiper from 'react-native-swiper';
 
 export default class Client extends Component {
-	constructor(){
-		super();
-		this.state = {
-			user: {
-				firstName: '',
-				middleName: '',
-				lastName: '',
-				contact: '',
-				eMail: '',
-				address: {
-					blk: '',
-					lot: '',
-					phase: ''
-				},
-				username: '',
-				password: '',
-				rePassword: '',
-				type: 'client'
-			}
-		}
-	}
-
-	static defaultProps = {
-		Phases: ['Ph 1','Ph 2','Ph 3','Ph 4']
-	};
-
-	static navigationOptions = {
-		title: 'Client Registration'
-	}
-
-	// Submit form
-	// handleSubmit = () => {
-	// 	const { firstName, middleName, lastName, contact, eMail, blk,lot,phase,username,password } = this.state;
-	// 	console.log(firstName, middleName, lastName, contact, eMail, blk,lot,phase,username,password, );
-	// 	if(firstName === '' && middleName === '' && lastName === '' && contact === '' && eMail === '' && blk === ''&& lot === '' && phase === ''&& username === '' && password === ''){
-	// 		alert('Please answer all the remaint text box');
-	// 	}
-	// }
+  constructor(){
+    super();
+    this.state = {
+       fName: null,
+       lName: null,
+       address: null,
+       ContactNumber: null,
+       Male: false,
+       Female: false,
+       username: null,
+       password: null,
+       confirmatoryPassword: null,
+       email: null,
+       checkmark: ''
+    }
+  }
+  static navigationOptions = {
+    title: 'Client Registration'
+  }
 
 
-	// note in the database only to category is accepted change the category designated to number of category
-	handleSubmit = () => {
-		var request = new XMLHttpRequest();
-		const { firstName, middleName, lastName, contact, eMail, blk,lot,phase,username,password } = this.state;
-		request.open('POST', 'http://192.168.0.15:5000/users/user', true);
-		let formdata = new FormData();
-		formdata.append(firstName, middleName, lastName, contact, eMail, blk,lot,phase,username,password)
-		request.send(formdata);
-		console.log(formdata);
-	}
+  handleMaleReg = () => {
+    this.setState({
+      Male: true,
+      Female: false
+    })
+  }
 
-	render(){
-		const phases = this.props.Phases.map(ph => {
-			return(
-				<Picker.Item key={ph} label={ph} value={ph}/>
-			)
-		})
-		return(
-			<ScrollView>
-				<View style={styles.container}>
-					<View style={{flex: 1,flexDirection: 'column', alignItems: 'center', justifyContent: 'space-between'}}>
-						<View style={{flexDirection: 'row', marginTop: 15, alignItems: 'center'}}>
-							<Text>
-								First Name: 
-							</Text>
-							<TextInput
-							onChangeText={(firstName) => this.setState({firstName})} 
-							placeholder="Input First Name"
-			 				style={{height: 40, width: '60%', backgroundColor: '#f2f2f2', borderColor: 'gray', borderWidth: 2, borderRadius: 20, textAlign: 'center'}}/>
-						</View>
-						<View style={{flexDirection: 'row', marginTop: 15, alignItems: 'center'}}>
-							<Text>
-								Middle Initial: 
-							</Text>
-							<TextInput
-							onChangeText={(middleName) => this.setState({middleName})} 
-							placeholder="Input Middle Initial"
-			 				style={{height: 40, width: '60%', backgroundColor: '#f2f2f2', borderColor: 'gray', borderWidth: 2, borderRadius: 20, textAlign: 'center'}}/>
-						</View>	
-						<View style={{flexDirection: 'row', marginTop: 15, alignItems: 'center'}}>
-							<Text>
-								Last Name: 
-							</Text>
-							<TextInput 
-							onChangeText={(lastName) => this.setState({lastName})}
-							placeholder="Input Last Name"
-			 				style={{height: 40, width: '60%', backgroundColor: '#f2f2f2', borderColor: 'gray', borderWidth: 2, borderRadius: 20, textAlign: 'center'}}/>
-						</View>
-						<View style={{flexDirection: 'row', marginTop: 15, alignItems: 'center'}}>
-							<Text>
-								Contact: 
-							</Text>
-							<TextInput 
-							onChangeText={(contact) => this.setState({contact})}
-							keyboardType='numeric'
-							maxLength={11}  //setting limit of input
-							placeholder="Input Contact"
-			 				style={{height: 40, width: '60%', backgroundColor: '#f2f2f2', borderColor: 'gray', borderWidth: 2, borderRadius: 20, textAlign: 'center'}}/>
-						</View>
-						<View style={{flexDirection: 'row', marginTop: 15, alignItems: 'center'}}>
-							<Text>
-								E-mail: 
-							</Text>
-							<TextInput 
-							onChangeText={(eMail) => this.setState({eMail})}
-							placeholder="Input E-mail"
-			 				style={{height: 40, width: '60%', backgroundColor: '#f2f2f2', borderColor: 'gray', borderWidth: 2, borderRadius: 20, textAlign: 'center'}}/>
-						</View>	
-		
-						<View style={{flexDirection: 'column', marginTop: 15, alignItems: 'center'}}>
-							<Text>Address of the Carlton Residence</Text>
-							<View style={{flexDirection: 'row', marginTop: 15, alignItems: 'center'}}>
-								<Text>BLK: </Text>
-								<TextInput
-								keyboardType='numeric'
-								maxLength={2}
-								onChangeText={(blk) => this.setState({blk})} 
-								placeholder="Input Block"
-				 				style={{height: 40, width: '60%', backgroundColor: '#f2f2f2', borderColor: 'gray', borderWidth: 2, borderRadius: 20, textAlign: 'center'}}/>
-							</View>
-							<View style={{flexDirection: 'row', marginTop: 15, alignItems: 'center'}}>
-								<Text>LOT: </Text>
-								<TextInput
-								keyboardType='numeric'
-								maxLength={2}
-								onChangeText={(lot) => this.setState({lot})} 
-								placeholder="Input Lot"
-				 				style={{height: 40, width: '60%', backgroundColor: '#f2f2f2', borderColor: 'gray', borderWidth: 2, borderRadius: 20, textAlign: 'center'}}/>
-							</View>
-							<Text>Select a phase</Text>
-							<View style={{flexDirection: 'row', marginTop: 15, alignItems: 'center'}}>
-								<Text>Phase: </Text>
-								<Picker selectedValue={this.state.phase}
-								onValueChange={(itemValue, itemIndex) => this.setState({phase: itemValue})}
-								style={{height: 50, width: 100}}>{phases}</Picker>
-							</View>
-						</View>
-						{/*User Info*/}
-						<View style={{flexDirection: 'row', marginTop: 15, alignItems: 'center'}}>
-							<Text>Username: </Text>
-							<TextInput
-							onChangeText={(username) => this.setState({username})} 
-							placeholder="Input Username"
-							style={{height: 40, width: '60%', backgroundColor: '#f2f2f2', borderColor: 'gray', borderWidth: 2, borderRadius: 20, textAlign: 'center'}}/>
-						</View>
-					{/*Conditional display*/}
-						<Text style={{textAlign: 'center'}}>Note:Conditional rendiring. Example User name has been taken</Text>
-					{/*Password*/}
-						<View style={{flexDirection: 'row', marginTop: 15, alignItems: 'center'}}>
-							<Text>Password: </Text>
-							<TextInput
-							onChangeText={(password) => this.setState({password})}  
-							placeholder="Input Password"
-							style={{height: 40, width: '60%', backgroundColor: '#f2f2f2', borderColor: 'gray', borderWidth: 2, borderRadius: 20, textAlign: 'center'}}/>
-						</View>
-					{/*Validity*/}
-						<View style={{alignItems: 'center', justifyContent: 'center'}}>
-							<Text style={{textAlign: 'center'}}>Re-type Password</Text>
-						</View>
-						<View style={{flexDirection: 'row', margin: 15, alignItems: 'center'}}>
-							<Text>Password: </Text>
-							<TextInput
-							onChangeText={(rePassword) => this.setState({rePassword})}  
-							placeholder="Re-type Password"
-							style={{height: 40, width: '60%', backgroundColor: '#f2f2f2', borderColor: 'gray', borderWidth: 2, borderRadius: 20, textAlign: 'center'}}/>
-						</View>
-					</View>
-					<View style={{marginBottom: 15}}>
-						<Button title="Submit" onPress={this.handleSubmit}/>
-					</View>
-				</View>
-			</ScrollView>
-		)
-	}
+  handleFemaleReg = () => {
+    this.setState({
+      Female: true,
+      Male: false
+    })
+  }
+
+  /*Submit event, Check for unfill up textbox, comparison check, validation*/
+  handleSubmit = () => {
+    const { navigate } = this.props.navigation;
+    this.props.navigation.navigate('Login')
+  }
+
+  render(){
+    let icon;
+    // Password Checking comparison
+      if(this.state.password === null && this.state.confirmatoryPassword === null){
+        icon = '';
+      }else if(this.state.password === this.state.confirmatoryPassword){
+        icon = 'checkmark-circle';
+      }else{
+        icon = 'close-circle'
+      }
+    return (
+      <Swiper style={styles.wrapper} showsButtons={false}>
+        <Container>
+        <Content>
+          <View style={{flex: 1, alignItems: 'center', marginTop: 20}}>
+            <Text style={{fontSize: 14}}>Step 1. Fill up First all Personal Information</Text>
+          </View>          
+          <Form>
+            <Item floatingLabel>
+              <Label>First Name</Label>
+              <Input onChangeText={(fName) => this.setState({fName: fName})}/>
+            </Item>
+            <Item floatingLabel last>
+              <Label>Last Name</Label>
+              <Input onChangeText={(lName) => this.setState({lName: lName})}/>
+            </Item>
+            <Item floatingLabel last>
+              <Label>Address</Label>
+              <Input onChangeText={(address) => this.setState({address: address})}/>
+            </Item>
+            <Item floatingLabel last>
+              <Label>Contact Number</Label>
+              <Input onChangeText={(number) => this.setState({ContactNumber: number})}/>
+            </Item>
+            <ListItem onPress={this.handleMaleReg}>
+                <Radio style={{paddingRight: 10}} selected={this.state.Male} onPress={this.handleMaleReg}/>
+                <TouchableOpacity >
+                  <Text>Male</Text>  
+                </TouchableOpacity>
+            </ListItem>
+            <ListItem onPress={this.handleFemaleReg}>
+                <Radio style={{paddingRight: 10}} selected={this.state.Female} onPress={this.handleFemaleReg} />
+                <TouchableOpacity >
+                  <Text>Female</Text>  
+                </TouchableOpacity>
+            </ListItem>
+          </Form>
+        </Content>
+      </Container>
+         <Container>
+        <Content>
+          <View style={{flex: 1, alignItems: 'center', marginTop: 20}}>
+            <Text style={{fontSize: 14}}>Step 2. User creation Information</Text>
+          </View>          
+          <Form>
+            <Item floatingLabel>
+              <Label>Username</Label>
+              <Input onChangeText={(username) => this.setState({username: username})}/>
+            </Item>
+            <Item floatingLabel last success>
+              <Label>Password</Label>
+              <Input onChangeText={(password) => this.setState({password: password})}/>
+            </Item>
+            <Item floatingLabel last>
+              <Label>Confirm Password</Label>
+              <Input onChangeText={(conPassword) => this.setState({confirmatoryPassword: conPassword})}/>
+              <Icon style={{color: 'red'}}name={icon}/>
+            </Item>
+            <Item floatingLabel last>
+              <Label>E-mail</Label>
+              <Input onChangeText={(email) => this.setState({email: email})}/>
+            </Item>
+          </Form>
+          <TouchableOpacity style={styles.button} onPress={this.handleSubmit.bind(this)}>
+            <Text style={{color: 'white', fontSize: 16, fontWeight: 'bold'}}>Submit</Text>
+          </TouchableOpacity>
+        </Content>
+      </Container>
+      
+      </Swiper>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center'
-	},
-	fontStyle: {
-		fontSize: 14,
-		fontWeight: 'bold',
-		fontStyle: 'italic'
-	}
+  wrapper: {
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'red',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BB',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    margin: 15,
+    padding: 15,
+      backgroundColor: "#00BFFF",
+      borderRadius: 10,
+    }
 })
